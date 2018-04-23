36b037246b91285d8639ed499a7c29c82620cf48
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ObtainHandlesOfAllRows/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace ObtainHandlesOfAllRows {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            grid.ItemsSource = new ProductList();
+            grid.GroupBy("Quantity");
+            UpdateListBoxes();
+        }
+        
+        private void TableView_FocusedRowChanged(object sender, DevExpress.Xpf.Grid.FocusedRowChangedEventArgs e) {
+            
+            UpdateListBoxes();
+        }
+
+        private void UpdateListBoxes() {
+            if (listInGroupRowHandles == null || listRowHandles == null) return;
+            listInGroupRowHandles.ItemsSource = GetDataRowHandlesInGroup(grid.View.FocusedRowHandle);
+            listRowHandles.ItemsSource = GetDataRowHandles();
+        }
+
+        private List<int> GetDataRowHandles() {
+            List<int> rowHandles = new List<int>();
+            for (int i = 0; i < grid.VisibleRowCount; i++) {
+                int rowHandle = grid.GetRowHandleByVisibleIndex(i);
+                if (grid.IsGroupRowHandle(rowHandle)) {
+                    if (!grid.IsGroupRowExpanded(rowHandle)) {
+                        rowHandles.AddRange(GetDataRowHandlesInGroup(rowHandle));
+                    }
+                }
+                else
+                    rowHandles.Add(rowHandle);
+            }
+            return rowHandles;
+        }
+        private List<int> GetDataRowHandlesInGroup(int handle) {
+            List<int> rowHandles = new List<int>();
+            if (grid.GroupCount == 0) return rowHandles;
+            int groupRowHandle = -1;
+            if (grid.IsGroupRowHandle(handle))
+                groupRowHandle = handle;
+            else
+                groupRowHandle = grid.GetParentRowHandle(handle);
+            for (int i = 0; i < grid.GetChildRowCount(groupRowHandle); i++) {
+                int rowHandle = grid.GetChildRowHandle(groupRowHandle, i);
+                if (grid.IsGroupRowHandle(rowHandle)) {
+                    rowHandles.AddRange(GetDataRowHandlesInGroup(rowHandle));
+                }
+                else
+                    rowHandles.Add(rowHandle);
+            }
+            return rowHandles;
+        }
+    }
+}
---
+++ VB/ObtainHandlesOfAllRows/MainPage.xaml.vb
@@ -0,0 +1,73 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Net
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Animation
+Imports System.Windows.Shapes
+
+Namespace ObtainHandlesOfAllRows
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+			grid.ItemsSource = New ProductList()
+			grid.GroupBy("Quantity")
+			UpdateListBoxes()
+		End Sub
+
+		Private Sub TableView_FocusedRowChanged(ByVal sender As Object, ByVal e As DevExpress.Xpf.Grid.FocusedRowChangedEventArgs)
+
+			UpdateListBoxes()
+		End Sub
+
+		Private Sub UpdateListBoxes()
+			If listInGroupRowHandles Is Nothing OrElse listRowHandles Is Nothing Then
+				Return
+			End If
+			listInGroupRowHandles.ItemsSource = GetDataRowHandlesInGroup(grid.View.FocusedRowHandle)
+			listRowHandles.ItemsSource = GetDataRowHandles()
+		End Sub
+
+		Private Function GetDataRowHandles() As List(Of Integer)
+			Dim rowHandles As New List(Of Integer)()
+			For i As Integer = 0 To grid.VisibleRowCount - 1
+				Dim rowHandle As Integer = grid.GetRowHandleByVisibleIndex(i)
+				If grid.IsGroupRowHandle(rowHandle) Then
+					If (Not grid.IsGroupRowExpanded(rowHandle)) Then
+						rowHandles.AddRange(GetDataRowHandlesInGroup(rowHandle))
+					End If
+				Else
+					rowHandles.Add(rowHandle)
+				End If
+			Next i
+			Return rowHandles
+		End Function
+		Private Function GetDataRowHandlesInGroup(ByVal handle As Integer) As List(Of Integer)
+			Dim rowHandles As New List(Of Integer)()
+			If grid.GroupCount = 0 Then
+				Return rowHandles
+			End If
+			Dim groupRowHandle As Integer = -1
+			If grid.IsGroupRowHandle(handle) Then
+				groupRowHandle = handle
+			Else
+				groupRowHandle = grid.GetParentRowHandle(handle)
+			End If
+			For i As Integer = 0 To grid.GetChildRowCount(groupRowHandle) - 1
+				Dim rowHandle As Integer = grid.GetChildRowHandle(groupRowHandle, i)
+				If grid.IsGroupRowHandle(rowHandle) Then
+					rowHandles.AddRange(GetDataRowHandlesInGroup(rowHandle))
+				Else
+					rowHandles.Add(rowHandle)
+				End If
+			Next i
+			Return rowHandles
+		End Function
+	End Class
+End Namespace
